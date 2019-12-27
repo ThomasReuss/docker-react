943b5f6fb18b1d1e5effd5790898090c7082a224
Thomas Reuß <thomas.reuss@online.de>
2019-12-27 13:43:37 +0000

    added feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there again and again. This is a new update!
+          Hi there again and again. This is a new update from feature branch!
         </p>
         <a
           className="App-link"
