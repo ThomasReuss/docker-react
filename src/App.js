f757d793e1dec3c3828bd9537c8965b786016d39
Thomas Reuß <thomas.reuss@online.de>
2019-12-27 13:37:58 +0000

    site update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there again and again!
+          Hi there again and again. This is a new update!
         </p>
         <a
           className="App-link"
